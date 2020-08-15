3eb994adc525ba526d03325ea90ee8a971321002
Walid Amriou <walidamriou@gmail.com>
2020-08-16 00:02:27 +0200

    001_Getting_Started/005_Implement_restart_button

---
+++ 001_Getting_Started/005_Implement_restart_button/src/main.c
@@ -0,0 +1,14 @@
+/*
+ ********************************************************************
+  Lokum ESP-IDF
+  To Learn using ESP-IDF (Espressif IoT Development Framework) to programming ESP32 boards with small programs examples
+  Project Website: LokumESPIDF.walidamriou.com
+  Github: https://github.com/walidamriou/LokumESPIDF
+
+  Copyright © 2020 Walid Amriou
+
+  Last update: August 2020
+ ********************************************************************
+*/
+
+void app_main() {}
