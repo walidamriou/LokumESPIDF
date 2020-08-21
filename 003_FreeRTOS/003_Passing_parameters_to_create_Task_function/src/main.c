b70351b5d5e4813863fdbec2e0c789498cd5de15
Walid Amriou <walidamriou@gmail.com>
2020-08-21 12:46:13 +0200

    add 003_FreeRTOS/003_Passing_parameters_to_create_Task_function/

---
+++ 003_FreeRTOS/003_Passing_parameters_to_create_Task_function/src/main.c
@@ -0,0 +1,63 @@
+/*
+ ********************************************************************
+  Lokum ESP-IDF
+  To Learn using ESP-IDF (Espressif IoT Development Framework) to programming ESP32 boards with small programs examples
+  Project Website: LokumESPIDF.walidamriou.com
+  Github: https://github.com/walidamriou/LokumESPIDF
+
+  Copyright © 2020 Walid Amriou
+
+  Last update: August 2020
+ ********************************************************************
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_err.h"
+
+static const char* TAG1 = "LOG1"; 
+// we will pass "Task 1" string to Task_1 function
+static const char* text_output_task1="Task 1";
+
+// The function of Task 1 
+void task_1(void *params);
+
+void app_main() {
+    // wait 3 s 
+    vTaskDelay(3000 / portTICK_PERIOD_MS);  
+    // for logging 
+    esp_log_level_set(TAG1, ESP_LOG_VERBOSE); 
+    // Create Tasks
+    // xTaskCreate() should return "pdTRUE" to indicate that the task was created successfully 
+    xTaskCreate(&task_1, // pointer to the task function
+                "Task 1", // text name for debugging only
+                2048, // Stack depth 
+                (void*)text_output_task1, // We pass parameters from main to the task function
+                1, // priority 1
+                NULL // We aren't using the task handle 
+               );
+
+    // If you use FreeRTOS with other board (STM32) you will call vTaskStartScheduler() here,
+    // but don't call it with ESP32, because this function is called before app_main starts. 
+    // In fact, app_main runs within a FreeRTOS task already!
+
+}
+
+// The function of Task 1 
+void task_1(void *params){
+    char *text_output;
+    text_output = (char*) params;
+    
+    // most tasks are implemented in an infinite loop
+while (true){
+    // we use logging info to see the task text
+    ESP_LOGI(TAG1, "%s",text_output);
+    // delay of 3s to print the next text
+    // portTICK_PERIOD_MS = 10
+    // 3000 / portTICK_PERIOD_MS = 300 ticks
+    // 300 * 10ms = 3s
+    vTaskDelay(3000 / portTICK_PERIOD_MS);  
+}
+}
